4f350eb8928f618f17fa24e38d03a848c6c215a2
Fachrul Rozi Hamonangan Harahap <85205921+fachrul-rh@users.noreply.github.com>
2022-01-03 21:15:04 +0700

    npm script and deploy

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $('#mycarousel').carousel({ interval: 2000 });
+  $('#carouselButton').click(function () {
+    if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+      $('#mycarousel').carousel('pause');
+      $('#carouselButton').children('span').removeClass('fa-pause');
+      $('#carouselButton').children('span').addClass('fa-play');
+    } else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+      $('#mycarousel').carousel('cycle');
+      $('#carouselButton').children('span').removeClass('fa-play');
+      $('#carouselButton').children('span').addClass('fa-pause');
+    }
+  });
+
+  $('#loginModal').hide();
+  $('#login').click(function (e) {
+    e.preventDefault();
+    $('#loginModal').modal('show');
+  });
+
+  $('#reserveTable').hide();
+  $('.reserved').click(function (e) {
+    e.preventDefault();
+    $('#reserveTable').modal('show');
+  });
+});
